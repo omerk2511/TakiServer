ab352f147fb1d7479b59979be6544f42fa74190c
Omer Katz <omerk2511@gmail.com>
2020-09-22 18:43:10 +0300

    Implemented the actual Client and fixed errors in the server.

--- source/handlers/server.py
+++ source/handlers/server.py
@@ -1,14 +1,16 @@
 import socket
 
+from client import Client
+
 LISTEN_AMOUNT = 5
-TIMEOUT = 1
+SERVER_TIMEOUT = 0.5
 
 class Server(object):
     def __init__(self, ip, port):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._socket.settimeout(TIMEOUT)
+        self._socket.settimeout(SERVER_TIMEOUT)
 
         self._socket.bind((ip, port))
         self._socket.listen(LISTEN_AMOUNT)
@@ -18,22 +20,32 @@ class Server(object):
         print '[+] created the server successfully'
 
     def start(self):
-        self.run()
+        try:
+            self._run()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self._close()
+            print '[*] bye bye!'
 
-    def run(self):
+    def _run(self):
         while True:
             try:
                 client_sock, client_addr = self._socket.accept()
-                client = Client(client_sock, *client_addr)
+
+                client = Client(client_sock, self._clients, *client_addr)
+                client.start()
 
                 self._clients.append(client)
             except socket.timeout:
                 continue
-            except:
-                print '[-] unknown exception'
+            except Exception as e:
+                print '[-] unknown exception:', e
                 break
 
+    def _close(self):
         for client in self._clients:
             client.close()
+            client.join()
         
         self._socket.close()
