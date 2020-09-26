081d5b27f9669127e3cd6df03664cd5200b46068
itay62848 <itay62848@gmail.com>
2020-09-26 17:09:56 +0300

    Added basic request and response management

--- source/handlers/client.py
+++ source/handlers/client.py
@@ -4,6 +4,7 @@ from threading import Thread, Lock
 
 from ..requests.utils import validate_request
 
+
 BUFFER_SIZE = 1024
 CLIENT_TIMEOUT = 0.5
 
@@ -14,6 +15,29 @@ BAD_REQUEST = {
         "message": "Bad request"
     }
 }
+
+CREATE_GAME = {
+    "status": "success",
+    "args": {
+        "game_id":  "",
+        "jwt":      ""
+    }
+}
+
+
+class Response(object): 
+    def __init__(self, status, **kwargs): #each response has status and args
+        self.status = status
+        self.args = kwargs
+
+    def serialize(self):
+        return json.dumps(
+            {
+                "status": self.status,
+                "args": self.args
+            }
+        )
+
 
 class Client(Thread):
     def __init__(self, sock, clients, ip, port):
@@ -28,19 +52,42 @@ class Client(Thread):
         self._running = True
         self._socket_lock = Lock()
 
+        self.request_handler = {
+            "create_game" : self.create_game
+        }
+
         print '[*] %s:%d has connected to the server' % (ip, port)
 
     def run(self):
         while self._running:
             try:
                 request = self._recieve_request()
-                self._socket.send(json.dumps(request))
+                self.handle_request(request)
             except ValueError:
                 self._send_bad_request()
             except socket.timeout:
                 continue
             except socket.error:
                 return self.close()
+
+    def handle_request(self, request):
+        code = request['code']
+        args = request['args']
+        response = self.request_handler[code](**args)
+        self._socket.send(response)
+
+
+    def create_game(self, **kwargs):
+        lobby_name = kwargs.get('lobby_name')
+        player_name  =kwargs.get('player_name')
+        password = kwargs.get('password').strip()
+        if not password.strip(): password = None # if password field is empty
+        print '[*] %s created lobby %s' % (player_name,lobby_name)
+        game_id = '1' # random 
+        token = 'abcd' # jwt
+        response = Response(status = 'success', game_id = game_id, jwt = token)
+        return response.serialize()
+
 
     def close(self):
         self._clients.remove(self)
