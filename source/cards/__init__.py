80801b9d2d351271603087862fab566f6d167990
Omer Katz <omerk2511@gmail.com>
2020-10-14 22:54:20 +0300

    Added card management implementation. Oded will implement the Hand class.

---
+++ source/cards/__init__.py
@@ -0,0 +1,4 @@
+from card import Card
+from card_type import CardType
+from deck import Deck
+from hand import Hand
---
+++ source/cards/hand.py
@@ -0,0 +1,6 @@
+HAND_INITIAL_SIZE = 8
+
+
+class Hand(object):
+    def __init__(self, cards):
+        self.cards = cards
