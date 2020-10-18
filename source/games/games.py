6a10953782908c143c7123b2becd8f4e7e675ab1
Omer Katz <omerk2511@gmail.com>
2020-10-18 18:31:06 +0300

    Made some minor modifications to games/games.py.

--- source/games/games.py
+++ source/games/games.py
@@ -6,6 +6,14 @@ from ..common import TakiException, Status
 games = {}
 
 
+def find_game(game_id):
+    if game_id not in games:
+        raise TakiException(Status.NOT_FOUND,
+                            'A game with the supplied game ID was not found.')
+
+    return games[game_id]
+
+
 def create_game(name, password, host, sock):
     game = Game(get_game_id(), name, password, host, sock)
     games[game.id] = game
@@ -14,36 +22,16 @@ def create_game(name, password, host, sock):
 
 
 def join_game(player_name, game_id, password, sock):
-    if game_id not in games:
-        raise TakiException(Status.NOT_FOUND,
-                            'A game with the supplied game ID was not found.')
-
-    game = games[game_id]
-    game.add_player(player_name, password, sock)
+    find_game(game_id).add_player(player_name, password, sock)
 
 
 def leave_game(player_name, game_id):
-    if game_id not in games:
-        raise TakiException(Status.NOT_FOUND,
-                            'A game with the supplied game ID was not found.')
-
-    game = games[game_id]
-    game.remove_player(player_name)
+    find_game(game_id).remove_player(player_name)
 
 
 def start_game(game_id):
-    if game_id not in games:
-        raise TakiException(Status.NOT_FOUND,
-                            'A game with the supplied game ID was not found.')
-
-    game = games[game_id]
-    game.start()
+    find_game(game_id).start()
 
 
 def take_cards(player_name, game_id):
-    if game_id not in games:
-        raise TakiException(Status.NOT_FOUND,
-                            'A game with the supplied game ID was not found.')
-
-    game = games[game_id]
-    return game.take_cards(player_name)
+    return find_game(game_id).take_cards(player_name)
